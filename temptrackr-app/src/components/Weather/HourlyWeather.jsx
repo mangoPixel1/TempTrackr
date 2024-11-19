50d8981df88cd97fe4200c4cafdee93ef25c57b8
Miguel Cavada <70104762+mangoPixel1@users.noreply.github.com>
2024-11-19 13:44:33 -0700

    Filtered time array to current up to 24 hours

--- temptrackr-app/src/components/Weather/HourlyWeather.jsx
+++ temptrackr-app/src/components/Weather/HourlyWeather.jsx
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from "react";
 import classes from "./Weather.module.css";
 
+// Icons
+import Clear from "../Icons/Weather Codes/Clear";
+import PartlyCloudyDay from "../Icons/Weather Codes/PartlyCloudyDay";
+import Overcast from "../Icons/Weather Codes/Overcast";
+import Fog from "../Icons/Weather Codes/Fog";
+import Drizzle from "../Icons/Weather Codes/Drizzle";
+import Rain from "../Icons/Weather Codes/Rain";
+import FreezingRain from "../Icons/Weather Codes/FreezingRain";
+import Snow from "../Icons/Weather Codes/Snow";
+import Hail from "../Icons/Weather Codes/Hail";
+import Thunderstorm from "../Icons/Weather Codes/Thunderstorm";
+
 // Contexts
 import { useTheme } from "../../context/ThemeContext";
 import { useUnit } from "../../context/UnitContext";
@@ -12,9 +24,10 @@ function HourlyWeather() {
 
 	const [hourlyData, setHourlyData] = useState({});
 
-	const [times, setTimes] = useState([]);
-	const [temps, setTemps] = useState([]);
-	const [wmoCodes, setWmoCodes] = useState([]);
+	const [currentTime, setCurrentTime] = useState(new Date());
+	const [filteredTime, setFilteredTime] = useState([]);
+	const [filteredTemp, setFilteredTemp] = useState([]);
+	const [filteredWeatherCode, setFilteredWeatherCode] = useState([]);
 
 	const weatherCodeMap = {
 		0: "Clear",
@@ -45,6 +58,57 @@ function HourlyWeather() {
 		99: "Thunderstorm"
 	};
 
+	function getConditionIcon(weatherCode) {
+		switch (weatherCode) {
+			case 0:
+			case 1:
+				return <Clear />;
+				break;
+			case 2:
+				return <PartlyCloudyDay />;
+				break;
+			case 3:
+				return <Overcast />;
+				break;
+			case 45:
+			case 48:
+				return <Fog />;
+				break;
+			case 51:
+			case 53:
+			case 55:
+				return <Drizzle />;
+				break;
+			case 61:
+			case 63:
+			case 65:
+			case 80:
+			case 81:
+			case 82:
+				return <Rain />;
+				break;
+			case 66:
+			case 67:
+				return <FreezingRain />;
+				break;
+			case 71:
+			case 73:
+			case 75:
+			case 85:
+			case 86:
+				return <Snow />;
+				break;
+			case 77:
+				return <Hail />;
+				break;
+			case 95:
+			case 96:
+			case 99:
+				return <Snow />;
+				break;
+		}
+	}
+
 	useEffect(() => {
 		fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weather_code&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&forecast_days=3`)
 			.then(response => {
@@ -56,25 +120,46 @@ function HourlyWeather() {
 			.then(data => {
 				//console.log(data);
 				setHourlyData(data.hourly);
-				setTimes(data.hourly.time);
-				setTemps(data.hourly.temperature_2m);
-				setWmoCodes(data.hourly.weather_code);
 			})
 			.catch(error => console.error(error));
 	}, [latitude, longitude, cityName, unit]);
 
 	useEffect(() => {
-		console.log(hourlyData);
+		// Calculate the time 24 hours from now
+		const twentyFourHoursLater = new Date(currentTime.getTime() + 24 * 60 * 60 * 1000);
+
+		const filteredTimeArr =
+			hourlyData.time &&
+			hourlyData.time.filter(timeString => {
+				const time = new Date(timeString);
+
+				return time >= currentTime && time <= twentyFourHoursLater;
+			});
+
+		// figure out how to filter temp and weather codes
+
+		setFilteredTime(filteredTimeArr);
+
+		//const filteredTwentyFourHours = hourlyData.time.filter();
 	}, [hourlyData]);
+
+	function formatTime() {}
+
+	/* TODO:
+		DONE: get current date and time
+		parse time array
+		filter data to show only current time -> 24 hours later
+		format time
+		fix weather icons not displaying
+	*/
 
 	return (
 		<div className={classes.hourlyWeatherContainer}>
 			<h1>Hourly weather</h1>
 			<ul className={classes.hourlyForecast}>
-				{hourlyData &&
-					hourlyData.time &&
-					hourlyData.time.map((time, index) => {
-						return <li key={index}>{`${hourlyData.time[index]}: ${hourlyData.temperature_2m[index]} - ${weatherCodeMap[hourlyData.weather_code[index]]}`}</li>;
+				{filteredTime &&
+					filteredTime.map((time, index) => {
+						return <li key={index}>{time}</li>;
 					})}
 			</ul>
 		</div>
@@ -82,3 +167,26 @@ function HourlyWeather() {
 }
 
 export default HourlyWeather;
+
+/*
+
+return (
+		<div className={classes.hourlyWeatherContainer}>
+			<h1>Hourly weather</h1>
+			<ul className={classes.hourlyForecast}>
+				{hourlyData &&
+					hourlyData.time &&
+					hourlyData.time.map((time, index) => {
+						return (
+							<li key={index}>
+								<div>{`${hourlyData.time[index]}`}</div>
+								<div>{`${getConditionIcon(hourlyData.weather_code[index])}`}</div>
+								<div>{`${hourlyData.temperature_2m[index]}`}</div>
+							</li>
+						);
+					})}
+			</ul>
+		</div>
+	);
+
+*/
